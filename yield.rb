1771aeb6a636bfc5bc79ae7f4a798d5f172de771
abhikumar90 <abhi.kumar90@hotmail.com>
2015-09-06 18:29:57 +0530

    yield method in ruby

---
+++ yield.rb
@@ -0,0 +1,16 @@
+#In Ruby, methods may receive a code block to perform arbitrary segments of code. When a method expects a block, it invokes it by calling the YIELD function.
+class Person
+     def initialize(name)
+       @name = name
+     end
+
+    def do_with_name
+       yield(@name)
+    end
+  end
+
+# person = Person.new("Oscar")
+       
+ # person.do_with_name do |name|  
+   # puts "hii my name is #{name}"
+#  end 
